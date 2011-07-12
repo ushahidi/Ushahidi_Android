a465c9216ba65fd68eb401b9cf57652623bbdc60
eyedol <addhen@linuxmail.org>
2011-07-12 13:52:54 +0300

    Close 1204: Fixed image resize

--- src/com/ushahidi/android/app/ui/SeekBarPreference.java
+++ src/com/ushahidi/android/app/ui/SeekBarPreference.java
@@ -23,7 +23,6 @@ package com.ushahidi.android.app.ui;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
