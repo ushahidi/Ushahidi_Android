4709cb042bbaec90e5d1ad1ee496491d6f656337
eyedol <addhen@linuxmail.org>
2011-08-16 20:43:35 +0300

    Made it to generate pot files as well

--- tools/string_generator.py
+++ tools/string_generator.py
@@ -52,203 +52,19 @@ import os
 import sys
 
 from optparse import OptionParser
+from xml.dom.minidom import Node
 
 class StringGenerator:
     
     """ generate string.xml or csv.txt file """
     
-    def __init__(self,lang,source, destination):
-        self.languages= {"ab":"Abkhaz","aa":"Afar","af":"Afrikaans ",
-        "ak" :"Akan ",
-        "sq" :"Albanian ",
-        "am" :"Amharic ",
-        "ar" :"Arabic ",
-        "an" :"Aragonese ",
-        "hy" :"Armenian ",
-        "as" :"Assamese ",
-        "av" :"Avaric ",
-        "ae" :"Avestan ",
-        "ay" :"Aymara ",
-        "az" :"Azerbaijani ",
-        "bm" :"Bambara ",
-        "ba" :"Bashkir ",
-        "eu" :"Basque ",
-        "be" :"Belarusian ",
-        "bn" :"Bengali ",
-        "bh" :"Bihari ",
-        "bi" :"Bislama ",
-        "bs" :"Bosnian ",
-        "br" :"Breton ",
-        "bg" :"Bulgarian ",
-        "my" :"Burmese ",
-        "ca" :"Catalan; Valencian ",
-        "ch" :"Chamorro ",
-        "ce" :"Chechen ",
-        "ny" :"Chichewa; Chewa; Nyanja ",
-        "zh" :"Chinese ",
-        "cv" :"Chuvash ",
-        "kw" :"Cornish ",
-        "co" :"Corsican ",
-        "cr" :"Cree ",
-        "hr" :"Croatian ",
-        "cs" :"Czech ",
-        "da" :"Danish ",
-        "dv" :"Divehi; Dhivehi; Maldivian; ",
-        "nl" :"Dutch ",
-        "en" :"English ",
-        "eo" :"Esperanto ",
-        "et" :"Estonian ",
-        "ee" :"Ewe ",
-        "fo" :"Faroese ",
-        "fj" :"Fijian ",
-        "fi" :"Finnish ",
-         "suomi, suomen kieli "
-        "fr" :"French ",
-        "ff" :"Fula; Fulah; Pulaar; Pular ",
-        "gl" :"Galician ",
-        "ka" :"Georgian ",
-        "de" :"German ",
-        "el" :"Greek, Modern ",
-        "gn" :"Guaraní ",
-        "gu" :"Gujarati ",
-        "ht" :"Haitian; Haitian Creole ",
-        "ha" :"Hausa ",
-        "he" :"Hebrew",
-        "hz" :"Herero ",
-        "hi" :"Hindi ",
-        "ho" :"Hiri Motu ",
-        "hu" :"Hungarian ",
-        "ia" :"Interlingua ",
-        "id" :"Indonesian ",
-        "ie" :"Interlingue ",
-        "ga" :"Irish ",
-        "ig" :"Igbo ",
-        "ik" :"Inupiaq ",
-        "io" :"Ido ",
-        "is" :"Icelandic ",
-        "it" :"Italian ",
-        "iu" :"Inuktitut ",
-        "ja" :"Japanese ",
-        "jv" :"Javanese ",
-        "kl" :"Kalaallisut, Greenlandic ",
-        "kn" :"Kannada ",
-        "kr" :"Kanuri ",
-        "ks" :"Kashmiri ",
-        "kk" :"Kazakh ",
-        "km" :"Khmer ",
-        "ki" :"Kikuyu, Gikuyu ",
-        "rw" :"Kinyarwanda ",
-        "ky" :"Kirghiz, Kyrgyz ",
-        "kv" :"Komi ",
-        "kg" :"Kongo ",
-        "ko" :"Korean ",
-        "ku" :"Kurdish ",
-        "kj" :"Kwanyama, Kuanyama ",
-        "la" :"Latin ",
-        "lb" :"Luxembourgish, Letzeburgesch ",
-        "lg" :"Luganda ",
-        "li" :"Limburgish, Limburgan, Limburger ",
-        "ln" :"Lingala ",
-        "lo" :"Lao ",
-        "lt" :"Lithuanian ",
-        "lu" :"Luba-Katanga ",
-        "lv" :"Latvian ",
-        "gv" :"Manx ",
-        "mk" :"Macedonian ",
-        "mg" :"Malagasy ",
-        "ms" :"Malay ",
-        "ml" :"Malayalam ",
-        "mt" :"Maltese ",
-        "mi" :"Māori ",
-        "mr" :"Marathi (Marāṭhī) ",
-        "mh" :"Marshallese ",
-        "mn" :"Mongolian ",
-        "na" :"Nauru ",
-        "nv" :"Navajo, Navaho ",
-        "nb" :"Norwegian Bokmål ",
-        "nd" :"North Ndebele ",
-        "ne" :"Nepali ",
-        "ng" :"Ndonga ",
-        "nn" :"Norwegian Nynorsk ",
-        "no" :"Norwegian ",
-        "ii" :"Nuosu ",
-        "nr" :"South Ndebele ",
-        "oc" :"Occitan ",
-        "oj" :"Ojibwe, Ojibwa ",
-        "cu" :
-        "Old Church Slavonic, Church Slavic, Church Slavonic, Old Bulgarian, Old Slavonic ",
-        "om" :"Oromo ",
-        "or" :"Oriya ",
-        "os" :"Ossetian, Ossetic ",
-        "pa" :"Panjabi, Punjabi ",
-        "pi" :"Pāli ",
-        "fa" :"Persian ",
-        "pl" :"Polish ",
-        "ps" :"Pashto, Pushto ",
-        "pt" :"Portuguese ",
-        "qu" :"Quechua ",
-        "rm" :"Romansh ",
-        "rn" :"Kirundi ",
-        "ro" :"Romanian, Moldavian, Moldovan ",
-        "ru" :"Russian ",
-        "sa" :"Sanskrit (Saṁskṛta) ",
-        "sc" :"Sardinian ",
-        "sd" :"Sindhi ",
-        "se" :"Northern Sami ",
-        "sm" :"Samoan ",
-        "sg" :"Sango ",
-        "sr" :"Serbian ",
-        "gd" :"Scottish Gaelic; Gaelic ",
-        "sn" :"Shona ",
-        "si" :"Sinhala, Sinhalese ",
-        "sk" :"Slovak ",
-        "sl" :"Slovene ",
-        "so" :"Somali ",
-        "st" :"Southern Sotho ",
-        "es" :"Spanish; Castilian ",
-        "su" :"Sundanese ",
-        "sw" :"Swahili ",
-        "ss" :"Swati ",
-        "sv" :"Swedish ",
-        "ta" :"Tamil ",
-        "te" :"Telugu ",
-        "tg" :"Tajik ",
-        "th" :"Thai ",
-        "ti" :"Tigrinya ",
-        "bo" :"Tibetan Standard, Tibetan, Central ",
-        "tk" :"Turkmen ",
-        "tl" :"Tagalog ",
-        "tn" :"Tswana ",
-        "to" :"Tonga (Tonga Islands) ",
-        "tr" :"Turkish ",
-        "ts" :"Tsonga ",
-        "tt" :"Tatar ",
-        "tw" :"Twi ",
-        "ty" :"Tahitian ",
-        "ug" :"Uighur, Uyghur ",
-        "uk" :"Ukrainian ",
-        "ur" :"Urdu ",
-        "uz" :"Uzbek ",
-        "ve" :"Venda ",
-        "vi" :"Vietnamese ",
-        "vo" :"Volapük ",
-        "wa" :"Walloon ",
-        "cy" :"Welsh ",
-        "wo" :"Wolof ",
-        "fy" :"Western Frisian ",
-        "xh" :"Xhosa ",
-        "yi" :"Yiddish ",
-        "yo" :"Yoruba ",
-        "za" :"Zhuang, Chuang "}
-        
+    def __init__(self,source, destination):
+                
         self.source = source
         self.destination = destination
-        self.lang = lang
-        self.generate_string_xml(lang,source,destination)    
-
-    def generate_string_xml(self,lang,csvfile,destination):
+    def generate_string_xml(self,lang):
         """ generate strings.xml file from a csv file """
-        csv_data = csv.reader(open(csvfile,"rb"))  
+        csv_data = csv.reader(open(self.source,"rb"))  
         doc = xml.dom.minidom.Document()
         resources = doc.createElementNS(
             "urn:oasis:names:tc:xliff:document:1.2","resources")
@@ -261,13 +77,10 @@ class StringGenerator:
                 string.appendChild(string_text)
                 resources.appendChild(string)
 
-        directory = self.create_dir(lang,destination)
+        directory = self.create_dir(lang,self.destination)
         stringxml = open(directory+"/strings.xml","w")
         stringxml.writelines(doc.toprettyxml(indent="  "))
         stringxml.close()
-
-    #print doc.toprettyxml(indent="  ")             
-    #def generate_csv(self):
     
     def create_dir(self, lang, destination):
         """ create android value-lang directory """
@@ -285,25 +98,58 @@ class StringGenerator:
     def write_xml_to_screen(self,doc):
         xml.dom.ext.PrettyPrint(doc, file_obj)
 
-# def sync_files(self,oldfile, newflie):
+    def generate_pot_file(self):
+        xml_doc = xml.dom.minidom.parse(self.source)
+        destfile = self.destination + "/ushahidi-android.pot"
+
+        if os.path.exists(destfile):
+            os.remove(destfile)
+        f = open(destfile, 'w')
+
+        for node in xml_doc.getElementsByTagName("string"):
+            name = node.getAttribute("name")
+            for string_element in node.childNodes:
+                if string_element.nodeType == Node.TEXT_NODE:
+                    f.write('#Key: %s\n'% name)
+                    f.write('msgid "%s"\n'% string_element.data.rstrip('\r\n'))
+                    f.write('msgstr ""\n\n')
+        
+        """ Include plurals in the string """
+        p_elements = xml_doc.getElementsByTagName("plurals")
+        if len(p_elements) > 0:
+            f.write("##PLURALS##\n")
+            for p_element in p_elements:
+                item_elements = p_element.getElementsByTagName("item")
+                if len(item_elements) > 0:
+                    for item_element in item_elements:
+                        quantity = item_element.getAttribute("quantity")
+                        for item in item_element.childNodes:
+                            if item.nodeType == Node.TEXT_NODE:
+                                f.write('#Key: %s\n'%quantity)
+                                f.write('msgid "%s"\n'%item.data)
+                                f.write('msgstr ""\n\n')
+
+
+        f.close()
 
 def main(args,options,parser):
-    if len(args) < 3:
+    if len(args) < 2:
         parser.print_usage() 
         print "%s: " %(str(len(args)))
 
-    else:
-        stringgen = StringGenerator(args[0],args[1],args[2])
-#stringgen.create_dir('en',args[1])
+    elif len(args) == 2:
+        stringgen = StringGenerator(args[0],args[1])
+        if options.pot == True:
+            stringgen.generate_pot_file()
+    elif len(args) == 3:
+        stringgen = StringGenerator(args[1],args[2])
+        stringgen.generate_string_xml(args[0])
 
-        if options.pot == 'pot':
-            stringgen.create_dir('en',args[1])
+usage = "usage: %prog <language_2_letter_iso> <source_file> <destination_folder> or %prog [options] <source_file> <destination_file>"
 
-usage = "usage: %prog [options] <language_2_letter_iso> <source_file> <destination_folder>"
 parser = OptionParser(usage=usage)
-parser.add_option("-i","--import", action="store",
-        help="import csv file and generate string.xml file",dest="import")
-parser.add_option("-p","--pot",action="store",help="generate pot file from string.xml file", dest="pot")
+
+parser.add_option("-p","--pot",action="store_true",help="generate pot file from string.xml file", dest="pot")
 (options, args) = parser.parse_args()
 
 if __name__ == "__main__":
