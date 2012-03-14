6eb8a53075298989c1f73ae2cc68d3fbdd0315c9
eyedol <addhen@linuxmail.org>
2012-03-14 18:27:55 +0400

    Add checkin entity

---
+++ Core/src/com/ushahidi/android/app/entities/Checkin.java
@@ -0,0 +1,46 @@
+/**
+ ** Copyright (c) 2010 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ **/
+package com.ushahidi.android.app.entities;
+
+/**
+ * @author eyedol
+ *
+ */
+public class Checkin implements IDbEntity{
+
+    /* (non-Javadoc)
+     * @see com.ushahidi.android.app.entities.IDbEntity#getDbId()
+     */
+    @Override
+    public int getDbId() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see com.ushahidi.android.app.entities.IDbEntity#setDbId(int)
+     */
+    @Override
+    public void setDbId(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
