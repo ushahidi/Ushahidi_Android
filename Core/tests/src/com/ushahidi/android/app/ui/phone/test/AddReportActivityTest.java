5750c01198d597f63ea17b1d79e6bdd6a2bcaeb1
Henry Addo <addhen@linuxmail.org>
2012-10-31 22:31:01 +0000

    Add test case. Though broken at the moment

---
+++ Core/tests/src/com/ushahidi/android/app/ui/phone/test/AddReportActivityTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ushahidi.android.app.ui.phone.test;
+
+import com.ushahidi.android.app.ui.phone.AddReportActivity;
+
+/**
+ * @author eyedol
+ *
+ */
+public class AddReportActivityTest extends
+		ActivityInstrumentationTestCase2<AddReportActivity> {
+
+	public AddReportActivityTest() {
+		super(AddReportActivity.class);
+	}
+
+}
