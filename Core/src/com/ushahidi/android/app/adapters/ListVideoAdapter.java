df20d62a05b928686ed86735450a45ff314bfedc
Henry Addo <addhen@linuxmail.org>
2013-07-01 13:16:12 +0900

    Refactor list video adapter to use getview pattern for display content

--- Core/src/com/ushahidi/android/app/adapters/ListVideoAdapter.java
+++ Core/src/com/ushahidi/android/app/adapters/ListVideoAdapter.java
@@ -35,73 +35,73 @@ import com.ushahidi.android.app.models.ListReportVideoModel;
  */
 public class ListVideoAdapter extends BaseListAdapter<ListReportVideoModel> {
 
-	private ListReportVideoModel mListReportVideoModel;
+    private ListReportVideoModel mListReportVideoModel;
 
-	private List<ListReportVideoModel> items;
+    private List<ListReportVideoModel> items;
 
-	private int totalVideos;
+    private int totalVideos;
 
-	/**
-	 * @param context
-	 */
-	public ListVideoAdapter(Context context) {
-		super(context);
-	}
+    /**
+     * @param context
+     */
+    public ListVideoAdapter(Context context) {
+        super(context);
+    }
 
-	class Widgets extends com.ushahidi.android.app.views.View {
+    class Widgets {
 
-		public Widgets(View view) {
-			super(view);
-			this.video = (TextView) view.findViewById(R.id.report_video_webview);
-			this.total = (TextView) view.findViewById(R.id.video_total);
-		}
+        TextView video;
+        TextView total;
 
-		TextView video;
-		TextView total;
-	}
+        public Widgets(View view) {
+            this.video = (TextView) view.findViewById(R.id.report_video_webview);
+            this.total = (TextView) view.findViewById(R.id.video_total);
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.widget.Adapter#getView(int, android.view.View,
-	 * android.view.ViewGroup)
-	 */
-	@Override
-	public View getView(int position, View view, ViewGroup viewGroup) {
-		View row = inflater.inflate(R.layout.list_video_item, viewGroup, false);
-		Widgets widgets = (Widgets) row.getTag();
+    }
 
-		if (widgets == null) {
-			widgets = new Widgets(row);
-			row.setTag(widgets);
-		}
+    /*
+     * (non-Javadoc)
+     * @see android.widget.Adapter#getView(int, android.view.View,
+     * android.view.ViewGroup)
+     */
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        Widgets widgets;
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_video_item, null);
+            widgets = new Widgets(view);
+            view.setTag(widgets);
+        }
+        else {
+            widgets = (Widgets) view.getTag();
+        }
 
-		//widgets.video.loadUrl(getItem(position).getVideo());
-		widgets.video.setText(getItem(position).getVideo());
-		widgets.total.setText(context.getResources().getQuantityString(
-				R.plurals.no_of_videos, totalVideos, totalVideos));
-		return row;
-	}
+        // widgets.video.loadUrl(getItem(position).getVideo());
+        widgets.video.setText(getItem(position).getVideo());
+        widgets.total.setText(context.getResources().getQuantityString(
+                R.plurals.no_of_videos, totalVideos, totalVideos));
+        return view;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.ushahidi.android.app.adapters.BaseListAdapter#refresh(android.content
-	 * .Context)
-	 */
-	@Override
-	public void refresh() {
+    /*
+     * (non-Javadoc)
+     * @see
+     * com.ushahidi.android.app.adapters.BaseListAdapter#refresh(android.content
+     * .Context)
+     */
+    @Override
+    public void refresh() {
 
-	}
+    }
 
-	public void refresh(int reportId) {
-		mListReportVideoModel = new ListReportVideoModel();
-		final boolean loaded = mListReportVideoModel.load(reportId);
-		totalVideos = mListReportVideoModel.totalReportVideos();
-		if (loaded) {
-			items = mListReportVideoModel.getVideos();
-			this.setItems(items);
-		}
-	}
+    public void refresh(int reportId) {
+        mListReportVideoModel = new ListReportVideoModel();
+        final boolean loaded = mListReportVideoModel.load(reportId);
+        totalVideos = mListReportVideoModel.totalReportVideos();
+        if (loaded) {
+            items = mListReportVideoModel.getVideos();
+            this.setItems(items);
+        }
+    }
 }
