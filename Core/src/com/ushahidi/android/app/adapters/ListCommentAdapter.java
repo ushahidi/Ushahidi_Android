2e7979c146b4783f163de93690edcaff759bc7e3
Henry Addo <addhen@linuxmail.org>
2013-07-01 12:59:53 +0900

    Refactor list map adapter to use getview pattern for display content

--- Core/src/com/ushahidi/android/app/adapters/ListCommentAdapter.java
+++ Core/src/com/ushahidi/android/app/adapters/ListCommentAdapter.java
@@ -17,6 +17,7 @@
  ** Ushahidi developers at team@ushahidi.com.
  **
  **/
+
 package com.ushahidi.android.app.adapters;
 
 import java.util.List;
@@ -32,101 +33,98 @@ import com.ushahidi.android.app.util.Util;
 
 /**
  * @author eyedol
- * 
  */
 public class ListCommentAdapter extends BaseListAdapter<ListCommentModel> {
 
-	/**
-	 * @param context
-	 */
-	public ListCommentAdapter(Context context) {
-		super(context);
-	}
+    /**
+     * @param context
+     */
+    public ListCommentAdapter(Context context) {
+        super(context);
+    }
 
-	private ListCommentModel mListCommentModel;
+    private ListCommentModel mListCommentModel;
 
-	private List<ListCommentModel> items;
+    private List<ListCommentModel> items;
 
-	private int totalComments;
+    private int totalComments;
 
-	class Widgets extends com.ushahidi.android.app.views.View {
+    class Widgets {
+        TextView commentAuthor;
+        TextView commentDate;
+        TextView commentDescription;
+        TextView total;
 
-		public Widgets(View view) {
-			super(view);
-			this.commentAuthor = (TextView) view
-					.findViewById(R.id.comment_author);
-			this.commentDate = (TextView) view.findViewById(R.id.comment_date);
-			this.commentDescription = (TextView) view
-					.findViewById(R.id.comment_description);
-			this.total = (TextView) view.findViewById(R.id.comment_total);
+        public Widgets(View view) {
 
-		}
+            this.commentAuthor = (TextView) view
+                    .findViewById(R.id.comment_author);
+            this.commentDate = (TextView) view.findViewById(R.id.comment_date);
+            this.commentDescription = (TextView) view
+                    .findViewById(R.id.comment_description);
+            this.total = (TextView) view.findViewById(R.id.comment_total);
 
-		TextView commentAuthor;
-		TextView commentDate;
-		TextView commentDescription;
-		TextView total;
-	}
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.widget.Adapter#getView(int, android.view.View,
-	 * android.view.ViewGroup)
-	 */
-	@Override
-	public View getView(int position, View view, ViewGroup viewGroup) {
-		View row = inflater.inflate(R.layout.list_comment_item, viewGroup,
-				false);
-		Widgets widgets = (Widgets) row.getTag();
+    }
 
-		if (widgets == null) {
-			widgets = new Widgets(row);
-			row.setTag(widgets);
-		}
+    /*
+     * (non-Javadoc)
+     * @see android.widget.Adapter#getView(int, android.view.View,
+     * android.view.ViewGroup)
+     */
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        Widgets widgets;
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_comment_item, null);
+            widgets = new Widgets(view);
+            view.setTag(widgets);
+        } else {
+            widgets = (Widgets) view.getTag();
+        }
 
-		// FIXME: only show the first item for now. In the future only get one
-		// item
-		widgets.commentAuthor.setText(getItem(position).getCommentAuthor());
-		widgets.commentDate.setText(Util.formatDate("yyyy-MM-dd HH:mm:ss",
-				getItem(position).getCommentDate(), "MMM dd, yyyy"));
-		widgets.commentDescription.setText(getItem(position)
-				.getCommentDescription());
-		widgets.total.setText(context.getResources().getQuantityString(
-				R.plurals.no_of_comments, totalComments, totalComments));
-		return row;
-	}
+        // FIXME: only show the first item for now. In the future only get one
+        // item
+        widgets.commentAuthor.setText(getItem(position).getCommentAuthor());
+        widgets.commentDate.setText(Util.formatDate("yyyy-MM-dd HH:mm:ss",
+                getItem(position).getCommentDate(), "MMM dd, yyyy"));
+        widgets.commentDescription.setText(getItem(position)
+                .getCommentDescription());
+        widgets.total.setText(context.getResources().getQuantityString(
+                R.plurals.no_of_comments, totalComments, totalComments));
+        return view;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.ushahidi.android.app.adapters.BaseListAdapter#refresh(android.content
-	 * .Context)
-	 */
-	@Override
-	public void refresh() {
+    /*
+     * (non-Javadoc)
+     * @see
+     * com.ushahidi.android.app.adapters.BaseListAdapter#refresh(android.content
+     * .Context)
+     */
+    @Override
+    public void refresh() {
 
-	}
+    }
 
-	public void refresh(int reportId) {
-		mListCommentModel = new ListCommentModel();
-		final boolean loaded = mListCommentModel.load(reportId);
-		totalComments = mListCommentModel.totalComments();
-		if (loaded) {
-			items = mListCommentModel.getComments();
-			this.setItems(items);
-		}
-	}
+    public void refresh(int reportId) {
+        mListCommentModel = new ListCommentModel();
+        final boolean loaded = mListCommentModel.load(reportId);
+        totalComments = mListCommentModel.totalComments();
+        if (loaded) {
+            items = mListCommentModel.getComments();
+            this.setItems(items);
+        }
+    }
 
-	public void refreshCheckinComment(int checkinId) {
-		mListCommentModel = new ListCommentModel();
-		final boolean loaded = mListCommentModel.loadCheckinComment(checkinId);
-		totalComments = mListCommentModel.totalComments();
-		if (loaded) {
-			items = mListCommentModel.getComments();
-			this.setItems(items);
-		}
-	}
+    public void refreshCheckinComment(int checkinId) {
+        mListCommentModel = new ListCommentModel();
+        final boolean loaded = mListCommentModel.loadCheckinComment(checkinId);
+        totalComments = mListCommentModel.totalComments();
+        if (loaded) {
+            items = mListCommentModel.getComments();
+            this.setItems(items);
+        }
+    }
 
 }
