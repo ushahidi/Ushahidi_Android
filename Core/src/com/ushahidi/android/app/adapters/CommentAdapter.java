c7e91d5306fcebf7b7fd45e4d1dfa09f3198744c
Henry Addo <addhen@linuxmail.org>
2013-07-01 12:39:31 +0900

    Refactor comment adapter to use getview pattern for display content

--- Core/src/com/ushahidi/android/app/adapters/CommentAdapter.java
+++ Core/src/com/ushahidi/android/app/adapters/CommentAdapter.java
@@ -17,6 +17,7 @@
  ** Ushahidi developers at team@ushahidi.com.
  **
  **/
+
 package com.ushahidi.android.app.adapters;
 
 import java.util.List;
@@ -32,98 +33,99 @@ import com.ushahidi.android.app.util.Util;
 
 /**
  * @author eyedol
- * 
  */
 public class CommentAdapter extends BaseListAdapter<ListCommentModel> {
 
-	private ListCommentModel mListCommentModel;
+    private ListCommentModel mListCommentModel;
 
-	private List<ListCommentModel> items;
+    private List<ListCommentModel> items;
 
-	private int[] colors;
+    private int[] colors;
 
-	/**
-	 * @param context
-	 */
-	public CommentAdapter(Context context) {
-		super(context);
-		colors = new int[] { R.drawable.odd_row_rounded_corners,
-				R.drawable.even_row_rounded_corners };
-	}
+    /**
+     * @param context
+     */
+    public CommentAdapter(Context context) {
+        super(context);
+        colors = new int[] {
+                R.drawable.odd_row_rounded_corners,
+                R.drawable.even_row_rounded_corners
+        };
+    }
 
-	class Widgets extends com.ushahidi.android.app.views.View {
+    class Widgets {
+        TextView commentAuthor;
+        TextView commentDate;
+        TextView commentDescription;
 
-		public Widgets(View view) {
-			super(view);
-			this.commentAuthor = (TextView) view
-					.findViewById(R.id.comment_author);
-			this.commentDate = (TextView) view.findViewById(R.id.comment_date);
-			this.commentDescription = (TextView) view
-					.findViewById(R.id.comment_description);
+        public Widgets(View view) {
+            this.commentAuthor = (TextView) view
+                    .findViewById(R.id.comment_author);
+            this.commentDate = (TextView) view.findViewById(R.id.comment_date);
+            this.commentDescription = (TextView) view
+                    .findViewById(R.id.comment_description);
 
-		}
+        }
 
-		TextView commentAuthor;
-		TextView commentDate;
-		TextView commentDescription;
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.widget.Adapter#getView(int, android.view.View,
-	 * android.view.ViewGroup)
-	 */
-	@Override
-	public View getView(int position, View view, ViewGroup viewGroup) {
-		int colorPosition = position % colors.length;
-		View row = inflater.inflate(R.layout.comment_item, viewGroup, false);
-		row.setBackgroundResource(colors[colorPosition]);
-		Widgets widgets = (Widgets) row.getTag();
+    /*
+     * (non-Javadoc)
+     * @see android.widget.Adapter#getView(int, android.view.View,
+     * android.view.ViewGroup)
+     */
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        int colorPosition = position % colors.length;
+        Widgets widgets;
 
-		if (widgets == null) {
-			widgets = new Widgets(row);
-			row.setTag(widgets);
-		}
+        if (view == null) {
+            view = inflater.inflate(R.layout.comment_item, null);
+            widgets = new Widgets(view);
+            view.setTag(widgets);
+        }
+        else {
+            widgets = (Widgets) view.getTag();
+        }
+        view.setBackgroundResource(colors[colorPosition]);
 
-		// FIXME: only show the first item for now. In the future only get one
-		// item
-		widgets.commentAuthor.setText(getItem(position).getCommentAuthor());
-		widgets.commentDate.setText(Util.formatDate("yyyy-MM-dd HH:mm:ss",
-				getItem(position).getCommentDate(), "MMM dd, yyyy"));
-		widgets.commentDescription.setText(getItem(position)
-				.getCommentDescription());
-		return row;
-	}
+        // FIXME: only show the first item for now. In the future only get one
+        // item
+        widgets.commentAuthor.setText(getItem(position).getCommentAuthor());
+        widgets.commentDate.setText(Util.formatDate("yyyy-MM-dd HH:mm:ss",
+                getItem(position).getCommentDate(), "MMM dd, yyyy"));
+        widgets.commentDescription.setText(getItem(position)
+                .getCommentDescription());
+        return view;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.ushahidi.android.app.adapters.BaseListAdapter#refresh(android.content
-	 * .Context)
-	 */
-	@Override
-	public void refresh() {
+    /*
+     * (non-Javadoc)
+     * @see
+     * com.ushahidi.android.app.adapters.BaseListAdapter#refresh(android.content
+     * .Context)
+     */
+    @Override
+    public void refresh() {
 
-	}
+    }
 
-	public void refresh(int reportId) {
-		mListCommentModel = new ListCommentModel();
-		final boolean loaded = mListCommentModel.load(reportId);
-		if (loaded) {
-			items = mListCommentModel.getCommentsByReportId(reportId);
-			this.setItems(items);
-		}
-	}
+    public void refresh(int reportId) {
+        mListCommentModel = new ListCommentModel();
+        final boolean loaded = mListCommentModel.load(reportId);
+        if (loaded) {
+            items = mListCommentModel.getCommentsByReportId(reportId);
+            this.setItems(items);
+        }
+    }
 
-	public void refreshCheckinComment(int checkinId) {
-		mListCommentModel = new ListCommentModel();
-		final boolean loaded = mListCommentModel.loadCheckinComment(checkinId);
-		if (loaded) {
-			items = mListCommentModel.getCommentsByCheckinId(checkinId);
-			this.setItems(items);
-		}
-	}
+    public void refreshCheckinComment(int checkinId) {
+        mListCommentModel = new ListCommentModel();
+        final boolean loaded = mListCommentModel.loadCheckinComment(checkinId);
+        if (loaded) {
+            items = mListCommentModel.getCommentsByCheckinId(checkinId);
+            this.setItems(items);
+        }
+    }
 
 }
--- Core/src/com/ushahidi/android/app/adapters/ListMapAdapter.java
+++ Core/src/com/ushahidi/android/app/adapters/ListMapAdapter.java
@@ -40,8 +40,11 @@ public class ListMapAdapter extends BaseListAdapter<ListMapModel> implements
 	public View getView(int position, View view, ViewGroup viewGroup) {
 		Preferences.loadSettings(context);
 		final int mapId = Preferences.activeDeployment;
-		View row = inflater.inflate(R.layout.list_map_item, viewGroup, false);
+		
 		int colorPosition = position % colors.length;
+		if( view == null) {
+		    view = inflater.inflate(R.layout.list_map_item, null);
+		}
 		row.setBackgroundResource(colors[colorPosition]);
 		
 		Widgets widget = (Widgets) row.getTag();
