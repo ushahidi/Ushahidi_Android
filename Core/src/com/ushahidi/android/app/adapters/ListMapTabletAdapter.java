00366f0bb672f7bd1a0b209c5d38b32cb9922770
Henry Addo <addhen@linuxmail.org>
2013-07-01 12:56:59 +0900

    Refactor tablet map adapter to use getview pattern for display content

--- Core/src/com/ushahidi/android/app/adapters/ListMapTabletAdapter.java
+++ Core/src/com/ushahidi/android/app/adapters/ListMapTabletAdapter.java
@@ -1,3 +1,4 @@
+
 package com.ushahidi.android.app.adapters;
 
 import java.util.ArrayList;
@@ -16,115 +17,118 @@ import com.ushahidi.android.app.R;
 import com.ushahidi.android.app.models.ListMapModel;
 
 public class ListMapTabletAdapter extends BaseListAdapter<ListMapModel>
-		implements Filterable {
+        implements Filterable {
 
-	private int[] colors;
+    private int[] colors;
 
-	private ListMapModel listMapModel;
+    private ListMapModel listMapModel;
 
-	public ListMapTabletAdapter(Context context) {
-		super(context);
-		listMapModel = new ListMapModel();
-		colors = new int[] { R.drawable.odd_row_rounded_corners,
-				R.drawable.even_row_rounded_corners };
-	}
+    public ListMapTabletAdapter(Context context) {
+        super(context);
+        listMapModel = new ListMapModel();
+        colors = new int[] {
+                R.drawable.odd_row_rounded_corners,
+                R.drawable.even_row_rounded_corners
+        };
+    }
 
-	@Override
-	// Use Context instead of FragmentActivity
-	public void refresh() {
-		final boolean loaded = listMapModel.load();
-		if (loaded) {
-			this.setItems(listMapModel.getMaps());
-		}
-	}
+    @Override
+    // Use Context instead of FragmentActivity
+    public void refresh() {
+        final boolean loaded = listMapModel.load();
+        if (loaded) {
+            this.setItems(listMapModel.getMaps());
+        }
+    }
 
-	public View getView(int position, View view, ViewGroup viewGroup) {
-		Preferences.loadSettings(context);
-		final int mapId = Preferences.activeDeployment;
-		View row = inflater.inflate(R.layout.list_map_item, viewGroup, false);
-		int colorPosition = position % colors.length;
-		row.setBackgroundResource(colors[colorPosition]);
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        Preferences.loadSettings(context);
+        final int mapId = Preferences.activeDeployment;
 
-		Widgets widget = (Widgets) row.getTag();
+        int colorPosition = position % colors.length;
+        Widgets widget;
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_map_item, null);
+            widget = new Widgets(view);
+            view.setTag(widget);
+        }
+        else {
+            widget = (Widgets) view.getTag();
+        }
+        view.setBackgroundResource(colors[colorPosition]);
 
-		if (widget == null) {
-			widget = new Widgets(row);
-			row.setTag(widget);
-		}
+        // initialize view with content
+        widget.mapName.setText(getItem(position).getName());
+        widget.mapDesc.setText(getItem(position).getDesc());
+        widget.mapUrl.setText(getItem(position).getUrl());
+        widget.mapId.setText(String.valueOf(getItem(position).getId()));
 
-		// initialize view with content
-		widget.mapName.setText(getItem(position).getName());
-		widget.mapDesc.setText(getItem(position).getDesc());
-		widget.mapUrl.setText(getItem(position).getUrl());
-		widget.mapId.setText(String.valueOf(getItem(position).getId()));
+        if (getItem(position).getId() == mapId) {
+            widget.arrow.setImageResource(R.drawable.selected);
+        }
 
-		if (getItem(position).getId() == mapId) {
-			widget.arrow.setImageResource(R.drawable.selected);
-		}
+        return view;
+    }
 
-		return row;
-	}
+    public class Widgets {
 
-	public class Widgets extends com.ushahidi.android.app.views.View {
+        TextView mapName;
 
-		TextView mapName;
+        TextView mapDesc;
 
-		TextView mapDesc;
+        TextView mapUrl;
 
-		TextView mapUrl;
+        TextView mapId;
 
-		TextView mapId;
+        ImageView arrow;
 
-		ImageView arrow;
+        public Widgets(View convertView) {
+            mapName = (TextView) convertView.findViewById(R.id.map_list_name);
+            mapDesc = (TextView) convertView.findViewById(R.id.map_list_desc);
+            mapUrl = (TextView) convertView.findViewById(R.id.map_list_url);
+            mapId = (TextView) convertView.findViewById(R.id.map_list_id);
+            arrow = (ImageView) convertView.findViewById(R.id.map_arrow);
+        }
+    }
 
-		public Widgets(View convertView) {
-			super(convertView);
-			mapName = (TextView) convertView.findViewById(R.id.map_list_name);
-			mapDesc = (TextView) convertView.findViewById(R.id.map_list_desc);
-			mapUrl = (TextView) convertView.findViewById(R.id.map_list_url);
-			mapId = (TextView) convertView.findViewById(R.id.map_list_id);
-			arrow = (ImageView) convertView.findViewById(R.id.map_arrow);
-		}
-	}
+    @Override
+    public Filter getFilter() {
+        return new MapFilter();
+    }
 
-	@Override
-	public Filter getFilter() {
-		return new MapFilter();
-	}
+    public class MapFilter extends Filter {
 
-	public class MapFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            results.values = items;
+            results.count = items.size();
 
-		@Override
-		protected FilterResults performFiltering(CharSequence constraint) {
-			FilterResults results = new FilterResults();
-			results.values = items;
-			results.count = items.size();
+            if (constraint != null && constraint.toString().length() > 0) {
+                constraint = constraint.toString().toLowerCase();
+                ArrayList<ListMapModel> filteredItems = new ArrayList<ListMapModel>();
+                ArrayList<ListMapModel> itemsHolder = new ArrayList<ListMapModel>();
+                itemsHolder.addAll(items);
+                for (ListMapModel map : itemsHolder) {
+                    if (map.getName().toLowerCase().contains(constraint)
+                            || map.getDesc().toLowerCase().contains(constraint)) {
+                        filteredItems.add(map);
+                    }
+                }
+                results.count = filteredItems.size();
+                results.values = filteredItems;
+            }
+            return results;
+        }
 
-			if (constraint != null && constraint.toString().length() > 0) {
-				constraint = constraint.toString().toLowerCase();
-				ArrayList<ListMapModel> filteredItems = new ArrayList<ListMapModel>();
-				ArrayList<ListMapModel> itemsHolder = new ArrayList<ListMapModel>();
-				itemsHolder.addAll(items);
-				for (ListMapModel map : itemsHolder) {
-					if (map.getName().toLowerCase().contains(constraint)
-							|| map.getDesc().toLowerCase().contains(constraint)) {
-						filteredItems.add(map);
-					}
-				}
-				results.count = filteredItems.size();
-				results.values = filteredItems;
-			}
-			return results;
-		}
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence constraint,
+                FilterResults results) {
+            List<ListMapModel> reports = (ArrayList<ListMapModel>) results.values;
+            setItems(reports);
 
-		@SuppressWarnings("unchecked")
-		@Override
-		protected void publishResults(CharSequence constraint,
-				FilterResults results) {
-			List<ListMapModel> reports = (ArrayList<ListMapModel>) results.values;
-			setItems(reports);
+        }
 
-		}
-
-	}
+    }
 }
